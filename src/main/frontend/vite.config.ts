c7e491c777b8ae51775a1d9887b85c9b6926a93a
brdv <david.baari@igtconsulting.eu>
2025-06-10 12:33:42 +0200

    vite.config fix

--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -5,23 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/MessagingHub',
-  server: {
-    proxy: {
-      '/messaging': {
-        target: 'http://localhost:5555',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  // uncomment for local development cors fix (port 5555 is the default for the WM IS)
+  // server: {
+  //   proxy: {
+  //     '/messaging': {
+  //       target: 'http://localhost:5555',
+  //       changeOrigin: true,
+  //       secure: false
+  //     }
+  //   }
+  // }
 })
 
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/MessagingHub',
-// })
