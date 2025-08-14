da2baf4e6a14b72c6a8c2c344c3efdb55ea57ce7
brdv <david.baari@igtconsulting.eu>
2025-08-14 14:04:41 +0200

    ++ kafka conneciton update

--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   plugins: [react()],
   base: '/MessagingHub',
   // uncomment for local development cors fix (port 5555 is the default for the WM IS)
-  // server: {
-  //   proxy: {
-  //     '/messaging': {
-  //       target: 'http://localhost:5555',
-  //       changeOrigin: true,
-  //       secure: false
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/messaging': {
+        target: 'http://localhost:5555',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
 
